554ada65a861b64b0326c2cbbe9f4a80f3ffc175
Kazuki USHIJIMA <41105246+kfs214@users.noreply.github.com>
2025-02-21 00:47:03 +0900

    regenerate with typescript template

--- src/handlers/get-weather-forecast.mjs
+++
@@ -1,8 +0,0 @@
-export const getWeatherForecastHandler = async () => {
-  // Boilerplate code
-  console.log('Getting weather forecast and sending a message via LINE');
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Weather forecast sent!' }),
-  };
-};
